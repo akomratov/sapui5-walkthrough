8e1cd4a5c557d84d040ecd5ca77a665b384bc80b
akomratov <a.komratov@yandex.ru>
2017-12-06 08:23:24 +0300

    step_15 - Nested views

---
+++ step_15/controller/HelloPanel.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+   "use strict";
+   return Controller.extend("demo.wt.controller.HelloPanel", {
+	   oData: { target: { name: "Target model" } },
+	   onDemoButtonPress: function() {
+		   var oResBundle = this.getView().getModel("i18n").getResourceBundle();
+		   var sTargetName = this.getView().getModel().getProperty("/target/name");
+		   var sMessage = oResBundle.getText("messageGreeting", sTargetName);
+		   MessageToast.show(sMessage);
+	   }
+   });
+});
